e2fe69d398c119de2c58ddd344aedc219278d603
Linda Chan <lindadesu09@gmail.com>
2014-12-16 13:37:38 +0800

    updated

--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,5 @@
  /* ==============================================
-================== Page Loader ===================
+Page Loader
 =============================================== */
 
     //<![CDATA[
@@ -10,9 +10,8 @@
     })
   //]]>
 
-
 /* ==============================================
-========== Navigation Scroll Effect =============
+Navigation Scroll Effect (Showing Fixed Nav bar)
 =============================================== */
 
 
@@ -35,15 +34,6 @@ $(document).ready(function () {
 
 });
 
-/* ==============================================
-Active Navigation Calling
-=============================================== */
-
-$('body').scrollspy({ 
-	target: '.nav-menu',
-	offset: 95
-})
-
 
 /* ==============================================
 Scroll Navigation
@@ -63,30 +53,6 @@ $(function() {
 		});
 	});
 
-/* ==============================================
-Drop Down Menu Fade Effect
-=============================================== */	
-
-$('.nav-toggle').hover(function() {
-	'use strict';
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(250);
-    }, function() {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(250)
- });
-
-/* ==============================================
-Mobile Menu Button
-=============================================== */	
-
-$('.mini-nav-button').click(function() {
-    $(".nav-menu").slideToggle("9000");
- });
-
-$('.nav a').click(function () {
-	if ($(window).width() < 970) {
-    	$(".nav-menu").slideToggle("2000")}
-	
-});
 
 /* ==============================================
 Wow Animation
@@ -101,106 +67,8 @@ Wow Animation
     wow.init();
 
 
-/* ==============================================
-Counter
-=============================================== */
-
-// Count elements
-    $('.count').each(function() {
-        var $this   = $(this);
-        $this.data('target', parseInt($this.html()));
-        $this.data('counted', false);
-        $this.html('0');
-    });
-    $(window).bind('scroll', function() {
-        var speed   = 3000;
-        $('.count').each(function() {
-            var $this   = $(this);
-            if(!$this.data('counted') && $(window).scrollTop() + $(window).height() >= $this.offset().top) {
-                $this.data('counted', true);
-                $this.animate({dummy: 1}, {
-                    duration: speed,
-                    step:     function(now) {
-                        var $this   = $(this);
-                        var val     = Math.round($this.data('target') * now);
-                        $this.html(val);
-                        if(0 < $this.parent('.value').length) {
-                            $this.parent('.value').css('width', val + '%');
-                        }
-                    }
-                });
-            }
-        });
-    }).triggerHandler('scroll');
-
-/* ==============================================
-Testimonials
-=============================================== */
-
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
-});
-
-
-$(window).load(function() {
-  $('.flexslider-client').flexslider({
-    animation: "slide",
-    animationLoop: false,
-    itemWidth: 210,
-    itemMargin: 5
-  });
-});
-
 /*----------------------------------------
-MAGNIFIC POPUP VIMEO VIDEO
-----------------------------------------*/
-$(document).ready(function() {
-        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-            disableOn: false,
-            type: 'iframe',
-            iframe: {
-                markup: '<div class="mfp-iframe-scaler">'+
-                        '<div class="mfp-close"></div>'+
-                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                        '<div class="mfp-title">Some caption</div>'+
-                        '</div>',
-                patterns: {
-                    youtube: {
-                        index: 'youtube.com/',
-                        id: 'v=',
-                        src: 'http://www.youtube.com/embed/%id%?autoplay=1'
-                    },
-                    vimeo: {
-                        index: 'vimeo.com/',
-                        id: '/',
-                        src: 'http://player.vimeo.com/video/%id%?autoplay=1'
-                    },
-                    gmaps: {
-                        index: '//maps.google.',
-                        src: '%id%output=embed'
-                    }
-                },
-                srcAction: 'iframe_src',
-            },
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: false,
-
-            fixedContentPos: false
-        });
-    });
-
-/*----------------------------------------
-Screenshots AppShowcase
-----------------------------------------*/
-$(function() {
-        AppShowcase.init();
-      });
-
-/*----------------------------------------
-Required Attribute
+Required Attribute // form validation
 ----------------------------------------*/
 $('input').attr('required', true);
 
